863c5d0eeb0d6f7311cc786a8cb0aa6e7869e3bd
Sylvia Akoth Omollo <sylvia_omollo@gwu.edu>
2022-11-03 20:52:55 +0300

    Done

--- lib/song.rb
+++ lib/song.rb
@@ -3,8 +3,35 @@ class Song
   attr_accessor :name, :album
 
   def initialize(name:, album:)
+    @id = id
     @name = name
     @album = album
   end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS songs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      album TEXT
+    )
+    SQL
+    DB[:conn].execute(sql)   
+    end
+
+    def save
+      sql = <<-SQL
+        INSERT INTO songs (name, album)
+        VALUES (?, ?)
+      SQL
+  
+      DB[:conn].execute(sql, self.name, self.album)
+  
+    end
+
+    def self.create(name:, album:)
+      song = Song.new(name: name, album: album)
+      song.save
+    end
 
 end
